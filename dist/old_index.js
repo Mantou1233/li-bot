2d6484598e431d376f328e2f77313690c97ecdbb
Mantou1233 <rwkn100@gmail.com>
2022-11-21 13:10:22 +0800

    uh

--- dist/old_index.js
+++ dist/old_index.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const oicq_1 = require("oicq");
-const client = (0, oicq_1.createClient)(2814595278, {
+const client = (0, oicq_1.createClient)(2186103712, {
     platform: 2
 });
 client.on("system.online", async () => {
     console.log("Logged in!");
-    require("fs/promises").writeFile("./sb.json", JSON.stringify(await client.getRoamingStamp()));
 });
 client
     .on("system.login.qrcode", function (e) {
--- src/old_index.ts
+++ src/old_index.ts
@@ -2,16 +2,12 @@ import { createClient } from "oicq";
 
 import { IMessage } from "./core/typings";
 
-const client = createClient(2814595278, {
+const client = createClient(2186103712, {
 	platform: 2
 });
 
 client.on("system.online", async () => {
 	console.log("Logged in!");
-	require("fs/promises").writeFile(
-		"./sb.json",
-		JSON.stringify(await client.getRoamingStamp())
-	);
 });
 
 client
